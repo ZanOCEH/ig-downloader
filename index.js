07f0873f3bce93b383672f4cea10e858cf770cc4
xyzan <nazandmuliawan1@gmail.com>
2025-05-10 17:08:07 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,30 +1,24 @@
 import fetch from 'node-fetch';
 import config from './config.js';
-let jmlhHit = 0;
-
+let hitCount = 0;
 export default async function handler(req, res) {
-  if (req.method === 'GET' && req.query.endpoint === 'hitter') {
+  if (req.method === 'GET' && req.query.endpoint === 'hitcounter') {
     return res.status(200).json({ 
       creator: config.creator,
-      hitnya: jmlhHit,
+      total_hits: hitCount,
       status: 'success' 
     });
   }
-
   if (req.method !== 'GET') {
-    return res.status(405).json({ error: 'Method lain. Pakailah GET.' });
+    return res.status(405).json({ error: 'Method Not Allowed. Use GET.' });
   }
-
-  const urliG = req.query.url;
-
-  if (!urliG) {
+  const targetUrl = req.query.url;
+  if (!targetUrl) {
     return res.status(400).json({ error: 'Missing `url` query parameter.' });
   }
-
-  ++;
-
+  hitCount++;
   try {
-    const pon = await fetch('https://www.fastdl.live/api/search', {
+    const response = await fetch('https://www.fastdl.live/api/search', {
       method: 'POST',
       headers: {
         'accept': 'application/json, text/plain, */*',
@@ -42,27 +36,24 @@ export default async function handler(req, res) {
         'user-agent': 'Mozilla/5.0 (Linux; Android 13; Pixel 7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/135.0.0.0 Mobile Safari/537.36',
         'cookie': '_ga=GA1.1.409819991.1746865809; _ga_MDP0KWXQVY=GS2.1.s1746865809$o1$g1$t1746865821$j0$l0$h0'
       },
-      body: JSON.stringify({ url: urliG })
+      body: JSON.stringify({ url: targetUrl })
     });
-
-    if (!pon.ok) {
-      throw new Error(`API responded with status ${pon.status}`);
+    if (!response.ok) {
+      throw new Error(`API responded with status ${response.status}`);
     }
-
-    const data = await pon.json();
+    const data = await response.json();
     res.status(200).json({
       creator: config.creator,
       data: data,
-      hitnya2: jmlhHit,
+      hits: hitCount,
       status: 'success'
     });
-
   } catch (err) {
     res.status(500).json({ 
       creator: config.creator,
       error: 'Failed to fetch data from fastdl.live', 
       details: err.message,
-      hitnya2: jmlhHit
+      hits: hitCount
     });
   }
 }
